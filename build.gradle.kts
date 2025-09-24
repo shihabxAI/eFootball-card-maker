d0e014230f296dc6f6fd9684d84ac74bb2816209
shihabxAI <shihabxgemini@gmail.com>
2025-09-25 01:12:47 +0600

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.3" apply false // ✅ add this
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "efootball_card_maker_3"
+include(":app")
